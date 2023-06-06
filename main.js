6897e84e1c0a0821b344a3243ddf447d6e1d3404
Ulises <ulisees.rando@gmail.com>
2023-06-06 09:57:29 -0300

    finaliza preentrega JS

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ let gabinetes = 0;
 let tipoItem;
 let cantidadItem;
 
+let appOn = true;
 
+do{
 alert(`Bievenido a la app de stock`);
 
 let opcion = prompt(`Que accion desea realizar
@@ -28,8 +30,32 @@ if(opcion == AGREGAR){
     cantidadItem = prompt(`Indique cantidad`);
     agregarItem(tipoItem, cantidadItem);
 }else if(opcion == QUITAR){
-    quitarItem()
+    tipoItem = prompt(`Que item desea agregar
+    Monitor
+    Teclado
+    Mouse
+    Gabiente`);
+    cantidadItem = prompt(`Indique cantidad`);
+    quitarItem(tipoItem,cantidadItem);
+}else{
+    tipoItem = prompt(`Que item desea mostrar el stock
+    Monitor
+    Teclado
+    Mouse
+    Gabiente`);
+    recuentoStock(tipoItem);
 }
+
+continuidadApp = prompt(`Desea realizar otra accion? Si/No`)
+if(continuidadApp !== 'Si' ){
+    appOn = false;
+}
+
+}while(appOn = true);
+
+
+
+
 
 
 
@@ -52,15 +78,50 @@ function agregarItem(tipo, cantidad){
             alert(`Se agregaron ${cantidad} gabinetes`)
             break;    
         default:
-            alert('Ingrese un item valido, asegurese de escribir correctamente')
+            alert('Ingrese un item valido, asegurese de escribirlo correctamente')
             break;
     }
 };
 
-function quitarItem(){
-
+function quitarItem(tipo,cantidad){
+    switch(tipo){
+        case 'monitor':
+            monitores -= cantidad;
+            alert(`Se removieron ${cantidad} monitores`)
+            break;
+        case 'teclado':
+            teclados -= cantidad;
+            alert(`Se removieron ${cantidad} teclados`)
+            break;
+        case 'mouse':
+            mouses -= cantidad;
+            alert(`Se removieron ${cantidad} mouses`)
+            break;    
+        case 'gabinete':
+            gabinetes -= cantidad;
+            alert(`Se removieron ${cantidad} gabinetes`)
+            break;    
+        default:
+            alert('Ingrese un item valido, asegurese de escribirlo correctamente')
+            break;
+    }
 };
 
-function recuentoStock(){
-
+function recuentoStock(tipo){
+    switch(tipo){
+        case 'monitor':
+            alert(`Hay ${monitores} monitores disponibles`)
+            break;
+        case 'teclado':
+            alert(`Hay ${teclados} teclados disponibles`)
+            break;
+        case 'mouse':
+            alert(`Hay ${mouses} mouses disponibles`)
+            break;
+        case 'gabinete':
+            alert(`Hay ${gabinetes} gabinetes disponibles`)
+            break;
+        default:
+            alert(`Ingrese un item valido, asegurese de escribirlo correctamente`)
+    }
 };
