4d8aafe8b3b1a96da1ad76f1027bdccb997b3d39
Eliasur <ulisees.rando@gmail.com>
2023-06-29 17:00:46 -0300

    preentrega numero 2

---
+++ alumno.js
@@ -0,0 +1,19 @@
+class Alumno{
+    constructor(nombre,apellido,dni,edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+
+        this.infoAlumno =()=>{
+            alert(`
+               Nombre: ${this.nombre}
+               Apellido: ${this.apellido}
+               Dni: ${this.dni}
+               Edad: ${this.edad}
+               `)
+        }
+    }
+
+
+}
--- main.js
+++ main.js
@@ -1,129 +1,110 @@
-const AGREGAR = 1;
-const QUITAR = 2;
-const CONTAR = 3;
+const AGREGAR_ALUMNO = 1;
+const QUITAR_ALUMNO = 2;
+const MODIFICAR_ALUMNO = 3;
+const VER_INFORMACION = 4;
+const VER_ALUMNOS = 5;
+
+let appOn = false;
 
 
-let monitores = 0;
-let teclados = 0;
-let mouses = 0;
-let gabinetes = 0;
+let alumnos = [];
 
-let tipoItem;
-let cantidadItem;
-
-let appOn = true;
-
+               
+        alert('Bienvenido a la base de datos de alumnos!')
+               
 do{
-    
-alert(`Bievenido a la app de stock`);
+    let opcion = prompt(`Que accion desea realizar?
+    1- Agregar alumno
+    2- Quitar alumno
+    3- Modificar informacion de un alumno
+    4- Ver informacion de un alumno
+    5- Ver todos los alumnos
+    `)
 
-let opcion = prompt(`Que accion desea realizar
-1 - Agregar items a stock existente
-2 - Quitar items de stock 
-3 - Recuento de stock`);
+    if(opcion == AGREGAR_ALUMNO){
+       let nombreAlumno = prompt('Ingrese nombre de alumno')
+       let apellidoAlumno = prompt('Ingrese apellido de alumno');
+       let dniAlumno = parseInt(prompt('Ingrese DNI de alumno'));
+       let edadAlumno = parseInt(prompt('Ingrese edad del alumno'));
+       let alumnoNuevo = agregarAlumno(nombreAlumno,apellidoAlumno,dniAlumno,edadAlumno);
+       alumnos.push(alumnoNuevo);
+       console.log(alumnoNuevo);
+    }else if(opcion == QUITAR_ALUMNO){
+        let dniBorrar = parseInt(prompt('Ingrese el numero de DNI del alumno a remover'))
+        removerAlumno(dniBorrar);
+    }
+    else if(opcion == MODIFICAR_ALUMNO){
+        let dniAlumno = parseInt(prompt('Ingrese DNI del alumno'));
+        let alumno = encontrarAlumno(dniAlumno);
+        let opcionMod = parseInt(prompt(`Que dato desea modificar?
+                                        1- Nombre
+                                        2- Apellido
+                                        3- Dni
+                                        4- Edad`))
+        switch(opcionMod){
+            case 1:
+                let nuevoNombre = prompt('Ingrese nuevo valor para nombre');
+                alumno.nombre = nuevoNombre;
+                break;
+            case 2:
+                let nuevoApellido = prompt('Ingrese nuevo valor para apellido');
+                alumno.apellido = nuevoApellido;
+                break;
+            case 3:
+                let nuevoDni = parseInt(prompt('Ingrese nuevo valor para DNI'));
+                alumno.dni - nuevoDni;
+                break;
+            case 4:
+                let nuevaEdad = parseInt(prompt('Ingrese nuevo valor para edad'));
+                alumno.edad = nuevaEdad;
+                break;
+            default:
+                alert('Ingrese un valor correcto (SOLO NUMEROS DEL 1 al 4)')
+        }
+        alert('Se modifico el valor correctamente. A continuacion los nuevos valores');
+        alumno.infoAlumno();
 
-if(opcion == AGREGAR){
-    tipoItem = prompt(`Que item desea AGREGAR
-    monitor
-    teclado
-    mouse
-    gabinete`);
-    cantidadItem = parseInt(prompt(`Indique cantidad`));
-    agregarItem(tipoItem,cantidadItem);
-}else if(opcion == QUITAR){
-    tipoItem = prompt(`Que item desea REMOVER
-    monitor
-    teclado
-    mouse
-    gabinete`);
-    cantidadItem = parseInt(prompt(`Indique cantidad`));
-    quitarItem(tipoItem,cantidadItem);
-}else{
-    tipoItem = prompt(`Que item desea mostrar el stock
-    monitor
-    teclado
-    mouse
-    gabinete`);
-    recuentoStock(tipoItem);
-}
-
-let continuaApp = prompt(`Desea realizar otra accion? SI / NO`)
-if(continuaApp == 'no' || continuaApp == 'NO'){
-    appOn = false;
-}else{
-    appOn = true;
-}
+    }
+    else if(opcion == VER_INFORMACION){
+        let dniAlumno = parseInt(prompt('Ingrese el DNI del alumno'));
+        let alumno = encontrarAlumno(dniAlumno);
+        alumno.infoAlumno();
+    }
+    else if(opcion == VER_ALUMNOS){
+        for(let i = 0; i < alumnos.length; i++){
+            alumnos[i].infoAlumno();
+        }
+    }else{
+        alert('Ingrese un valor correcto porfavor')
+    }
 
 
+
+    let continuarApp = prompt('desea realizar otra accion? si / no');
+    if(continuarApp == 'no' || continuarApp == 'NO'){
+        appOn = false;
+    }else{
+        appOn = true;
+    }
 }while(appOn == true);
 
 
 
 
+//funciones 
+function agregarAlumno(nombre,apellido,dni,edad){
+    let alumno = new Alumno(nombre,apellido,dni,edad);
+    return alumno;
+}
 
-function agregarItem(tipo, cantidad){
-    switch(tipo){
-        case 'monitor':
-            monitores += cantidad;
-            alert(`Se agregaron ${cantidad} monitores`)
-            break;
-        case 'teclado':
-            teclados += cantidad;
-            alert(`Se agregaron ${cantidad} teclados`)
-            break;
-        case 'mouse':
-            mouses += cantidad;
-            alert(`Se agregaron ${cantidad} mouses`)
-            break;    
-        case 'gabinete':
-            gabinetes += cantidad;
-            alert(`Se agregaron ${cantidad} gabinetes`)
-            break;    
-        default:
-            alert('Ingrese un item valido, asegurese de escribirlo correctamente')
-            break;
-    }
-};
+function removerAlumno(dni){
+    let dniComparar = dni;
+    alumnos = alumnos.filter(x => x.dni !== dniComparar);
+}
 
-function quitarItem(tipo,cantidad){
-    switch(tipo){
-        case 'monitor':
-            monitores -= cantidad;
-            alert(`Se removieron ${cantidad} monitores`)
-            break;
-        case 'teclado':
-            teclados -= cantidad;
-            alert(`Se removieron ${cantidad} teclados`)
-            break;
-        case 'mouse':
-            mouses -= cantidad;
-            alert(`Se removieron ${cantidad} mouses`)
-            break;    
-        case 'gabinete':
-            gabinetes -= cantidad;
-            alert(`Se removieron ${cantidad} gabinetes`)
-            break;    
-        default:
-            alert('Ingrese un item valido, asegurese de escribirlo correctamente')
-            break;
-    }
-};
-
-function recuentoStock(tipo){
-    switch(tipo){
-        case 'monitor':
-            alert(`Hay ${monitores} monitores disponibles`)
-            break;
-        case 'teclado':
-            alert(`Hay ${teclados} teclados disponibles`)
-            break;
-        case 'mouse':
-            alert(`Hay ${mouses} mouses disponibles`)
-            break;
-        case 'gabinete':
-            alert(`Hay ${gabinetes} gabinetes disponibles`)
-            break;
-        default:
-            alert(`Ingrese un item valido, asegurese de escribirlo correctamente`)
-    }
-};
+function encontrarAlumno(dni){
+    let dniBuscar = dni;
+    let indice = alumnos.findIndex(obj => obj.dni === dniBuscar);
+    let alumnoEncontrado = alumnos[indice];
+    return alumnoEncontrado;
+}
