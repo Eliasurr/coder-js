681272a726cbabe091744121e6d70429756c372f
Ulises <ulisees.rando@gmail.com>
2023-07-28 16:15:04 -0300

    update 28/7

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ btnFormAgregar.addEventListener('click', function(e){
   e.preventDefault();
   //Dibuja formulario para agregar alumnos 
   dibujarFormAgregar()
-      const btnAgregarAlumno = document.getElementById('btnAgregarAlumno');
       //Captura boton de agregar generado por JS
+      const btnAgregarAlumno = document.getElementById('btnAgregarAlumno');
       btnAgregarAlumno.addEventListener('click',function(e){
         e.preventDefault();
         //Captura inputs
@@ -31,19 +31,46 @@ btnFormAgregar.addEventListener('click', function(e){
           //Los envia como parametros a la funcion de agregar alumno
           let nuevoAlumno = agregarAlumno(nombre,apellido,dni,edad);
           //Actualiza arreglo alumnos en localStorage
-          actualizarArrAlumnos(nuevoAlumno);     
+          actualizarArrAlumnos(nuevoAlumno);
+          dibujarTablaListaAlumnos()   
       });
 })
 
 
+//Evento remover alumno!
 btnFormRemover.addEventListener('click',function(){
   dibujarFormRemover();
+    //Captura boton de remover alumno generado por JS
+    const btnRemoverAlumno = document.getElementById('btnRemover');
+    btnRemoverAlumno.addEventListener('click', function(e){
+      e.preventDefault();
+      let dniRemover = document.getElementById('dniRemover').value;
+      let nuevoArr = removerAlumno(dniRemover);
+      actualizarArrAlumnos(nuevoArr);
+      dibujarTablaListaAlumnos();
+    })
 })
 
 
 
-//funciones
+//Evento editar alumno!
+btnFormEditar.addEventListener('click', function(e){
+  e.preventDefault();
+  dibujarFormEditar();
+    const btnEditar = document.getElementById('btnEditar');
+    btnEditar.addEventListener('click', function(e){
+      let dniBuscar = document.getElementById('dniBuscar').value;
+    })
+})
 
+
+
+
+dibujarTablaListaAlumnos() 
+
+
+
+//funciones
 //Obtiene arreglo, si no existe lo crea.
 function obtenerArregloAlumnos(){
   let data = localStorage.getItem('listadoAlumnos');
@@ -63,19 +90,21 @@ function actualizarArrAlumnos(arr){
   localStorage.setItem('listadoAlumnos',JSON.stringify(arr));
 }
 
-
+//Devuelve nuevo arreglo filtrado
 function removerAlumno(dni){
-    let dniComparar = dni;
-    alumnos = alumnos.filter(x => x.dni !== dniComparar);
+  let arrAlumnos = obtenerArregloAlumnos();
+  let dniRemover = dni;
+  let nuevoArr = arrAlumnos.filter(alumno => alumno.dni !== dniRemover);
+  return nuevoArr;
 }
 
-function encontrarAlumno(dni){
-    let dniBuscar = dni;
-    let indice = alumnos.findIndex(obj => obj.dni === dniBuscar);
-    let alumnoEncontrado = alumnos[indice];
-    return alumnoEncontrado;
-}
 
+//Busca alumno y trae sus propiedas
+function buscarAlumno(dni){
+  let arrAlumnos = obtenerArregloAlumnos();
+  let indice = arrAlumnos.findIndex(alumno => alumno.dni == dni);
+  return arrAlumnos[indice];
+}
 
 
 function dibujarFormAgregar(){
@@ -101,8 +130,39 @@ function dibujarFormRemover(){
   let formRemover = document.createElement('form');
   formRemover.classList.add('d-flex', 'flex-column');
   formRemover.innerHTML = `
-    <input type="number id="dniRemover" placeholder="Ingrese DNI de alumno a remover">
+    <legend class="text-center">REMOVER ALUMNO</legend>
+    <input type="number" id="dniRemover" placeholder="Ingrese DNI de alumno a remover">
     <button class="btn btn-danger mt-3" id="btnRemover">Remover alumno</button>
   `
   divForm.replaceChildren(formRemover);
 }
+
+function dibujarFormEditar(){
+  let formEditar = document.createElement('form');
+  formEditar.classList.add('d-flex', 'flex-column');
+  formEditar.innerHTML = `
+      <legend class="text-center">EDITAR INFO DE ALUMNO</legend>
+      <input type="text" id="dniBuscar" placeholder="Ingrese DNI de alumno a editar">
+      <button class="btn btn-primary mt-3" id="btnBuscar">BUSCAR</button>
+  `
+  divForm.replaceChildren(formEditar);
+}
+
+function dibujarTablaListaAlumnos(){
+  let arrAlumnos = obtenerArregloAlumnos();
+  let cuerpoTabla = document.getElementById('listadoAlumnosTabla');
+  cuerpoTabla.innerHTML = '';
+  arrAlumnos.map((alumno) =>{
+    cuerpoTabla.innerHTML += `
+      <td>${alumno.nombre}</td>
+      <td>${alumno.apellido}</td>
+      <td>${alumno.dni}</td>
+      <td>${alumno.edad}</td>
+    `
+  })
+}
+
+
+
+
+
